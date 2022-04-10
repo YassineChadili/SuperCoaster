234bd02e2acb9ebca000d57a950ade6272e72ce6
YassineChadili <yassinechadili12@outlook.com>
2022-04-10 14:41:28 +0200

    fix #4

--- SuperCoaster/karretjes.php
+++ SuperCoaster/karretjes.php
@@ -32,7 +32,7 @@
           <nav>
             <a href="index.php">Home</a>
             <a href="contructie.php">Contructie</a>
-
+            <a href="karretjes.php">Karretjes</a>
             <a href="contacten.php">Contacten</a>
             <a href="">Bedrijf zelf</a>
           </nav>
@@ -40,19 +40,25 @@
       </div>
       <div class="banner">
         <div class="banner-text">
-          <p>Karretjes pagina</p>
+          <p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy onderdelen, kan elk pretpark het chassis naar wens aanpassen. Wij hebben een frame ontwikkeld die wij zo klein mogelijk hebben gehouden, zodat de rest van de kar helemaal zelf door het team ontworpen en ontwikkeld kan worden. Ook leveren wij de digitale bestanden waar deze ontwerpen op gebaseerd kunnen worden, zodat het team zo snel mogelijk aan de slag kan gaan unieke ontwerpen te creëren. Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt. Zo zijn onze producten dus breed inzetbaar, en zorgen wij voor de beste vooruitgang in de achtbaantechniek van het afgelopen decennia.</p>
         </div>
-
-
     </header>
+    <main>
+        <div class="karretjes">
+          <div class="wrapper">
+            <h3><span>KAR</span>RETJES</h3>
+            <div class="contructie-items">
+              <div class="contructie-item">
+               <h4>Karretjes</h4>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+            </div>
+          </div>
+        </div>
+      </div>
 
-    <body>
-      <img src="img/kar1.jpg" alt="a" width="350">
-      <img src="img/kar2.jpg" alt="a" width="350">
-      <img src="img/3.jpg" alt="a" width="350">
-
-
-    </body>
+    
 
 
 
