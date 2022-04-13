e8f4cf0750e34f4b749e644c44ea1fe6806690bc
Aykut0234 <93645887+Aykut0234@users.noreply.github.com>
2022-04-13 14:24:43 +0200

    footer aangepast

--- SuperCoaster/footer.php
+++ SuperCoaster/footer.php
@@ -1,7 +1,11 @@
 <footer>
     <p>&copy; curio copyright, Aykut, Ahmed, Yassine, Joost.</p>
     <p>3215XE, BREDA - Hendrikstraat 42</p>
+
     <a href="">@Instagram</a>  <a href="">@Twitter</a>  <a href="">@Facebook</a>
+
+    <p><marquee behavior="scroll" direction="left" scrollamount="20"
+          >NIEUW GEOPEND!</marquee></p>
 
 
 
